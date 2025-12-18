c494cd7f513fedbc0e2a5c8637a83902d900fed7
Diiegooo1414 <diprilu2006@gmail.com>
2025-12-18 18:50:25 +0100

    Cambios de config

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var vite_1 = require("vite");
+var vite_2 = require("@tailwindcss/vite");
+exports.default = (0, vite_1.defineConfig)({
+    plugins: [
+        (0, vite_2.default)(),
+    ],
+});
